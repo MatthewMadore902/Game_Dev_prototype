e7d81acca00084a67a3b3cc293e0e90120ce2e26
MatthewMadore902 <90638212+MatthewMadore902@users.noreply.github.com>
2021-11-05 14:33:45 -0300

    camera

--- Assets/Scripts/FirstPersonCamera.cs
+++ Assets/Scripts/FirstPersonCamera.cs
@@ -27,7 +27,7 @@ public class FirstPersonCamera : MonoBehaviour
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-        transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
     }
 
